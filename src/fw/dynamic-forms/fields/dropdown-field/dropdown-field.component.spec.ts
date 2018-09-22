7a8bcbeabdf110300c5dffaffaeb04d567f4b45f
vasu potla <potlavasu@gmail.com>
2018-09-22 11:18:52 -0400

    simple tests are passing now

--- src/fw/dynamic-forms/fields/dropdown-field/dropdown-field.component.spec.ts
+++ src/fw/dynamic-forms/fields/dropdown-field/dropdown-field.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component } from '@angular/core';
 import { DropdownFieldComponent } from './dropdown-field.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 
 describe('DropdownFieldComponent', () => {
   let component: DropdownFieldComponent;
@@ -18,6 +18,10 @@ describe('DropdownFieldComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(DropdownFieldComponent);
     component = fixture.componentInstance;
+    const field:FieldDefinition  = {id:1, key:'test', type:'lookUp', options:[]};
+    const formControl = new FormControl(field);
+    component.field = field;
+    component.form = new FormGroup({test : formControl});
     fixture.detectChanges();
   });
 
