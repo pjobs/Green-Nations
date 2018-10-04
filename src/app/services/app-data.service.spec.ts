479360288cbb628de28d20b2130fb8affeb5e039
vasu potla <potlavasu@gmail.com>
2018-10-04 17:32:53 -0400

    rewrote service tests with best practices

--- src/app/services/app-data.service.spec.ts
+++ src/app/services/app-data.service.spec.ts
@@ -1,38 +1,50 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, async } from '@angular/core/testing';
 import { Observable } from 'rxjs/Observable';
 import { notEqual } from 'assert';
 import { first, single } from 'rxjs/operators';
-import { HttpClientModule, HTTP_INTERCEPTORS  } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppDataService } from './app-data.service';
 import { Country } from '../view-models/country';
 import { Countries } from '../helpers/countries-list';
 import { fakeBackendProvider } from '../helpers/fake-backend.interceptor';
 
-describe('AppDataService', () => {
-  let service: AppDataService;
+interface IContext {
+  service: AppDataService;
+  country: Country;
+}
+
+describe('AppDataService', function (this: IContext) {
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AppDataService, { provide: 'BASE_URL', useValue: 'http://localhost:4200' },fakeBackendProvider],
+      providers: [AppDataService, { provide: 'BASE_URL', useValue: 'http://localhost:4200' }, fakeBackendProvider],
       imports: [HttpClientModule]
     });
   });
 
-  it('should be created', inject([AppDataService],(service: AppDataService)=>{
-    expect(service).toBeTruthy();
+  beforeEach(inject([AppDataService], (service: AppDataService) => {
+    this.service = service;
   }));
 
-  it('getCountry should return the right country',inject([AppDataService],(service: AppDataService)=>{
-    service.getCountry(1).subscribe((c)=>{
-      expect(c).toBeTruthy();
-      return expect(c.id).toEqual(1);
+  it('should create', () => {
+    expect(this.service).toBeTruthy();
+  });
+
+  describe('.getCountry', () => {
+    beforeAll(() => this.service.getCountry(1).subscribe((c) => this.country = c));
+    it('should return a valid Country', () => expect(this.country).toBeTruthy());
+    it('should return a right Country', () => expect(this.country.id).toEqual(1));
+  });
+
+  describe('.createCountry', () => {
+    beforeEach(() => {
+      this.country = { id: 0, name: 'United States', epiIndex: 68.04 };
+      this.service.createCountry(this.country);
     });
-  }));
-   
-  it('createCountry should add a country', inject([AppDataService], (service: AppDataService) => {
-    const country: Country = { id:0, name: 'United States', epiIndex: 68.04 };
-    service.createCountry(country);
-    service.getCountry(country.id).subscribe((c)=>{
-      return expect(c).toEqual(country);
-    });
-  }));
+    it('should add a new Country', () => {
+      this.service.getCountry(this.country.id).subscribe((c) => {
+        return expect(c).toEqual(this.country);
+      });
+    })
+  });
 });
